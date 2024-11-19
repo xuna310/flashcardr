042fbadc83165cbca1d989d8ebbc8b13edcfb939
xuna310 <siles@prof.educacao.sp.gov.br>
2024-11-19 00:32:27 -0300

    Update perguntas.js

--- perguntas.js
+++ perguntas.js
@@ -4,11 +4,6 @@ criaCartao(
     'O Python é uma linguagem de programação'
 )
 
-criaCartao(
-    'Geografia',
-    'Qual a capital da França?',
-    'A capital da França é Paris'
-)
 
 criaCartão(
     'Matemática',
